b9732839baf4992a3b89783a16473d86983a95fe
SuperDuper342 <lawrencemilo342@gmail.com>
2024-04-04 01:07:31 -0600

    finalize photos

--- src/components/GalleryYear3.vue
+++ src/components/GalleryYear3.vue
@@ -42,14 +42,15 @@
         <p class="font-weight-medium text-h4 text--primary mt-12 text-center">Events</p>
         <v-row class="text-center mb-12">
             <v-col class="text-center d-flex justify-space-around"
-                   v-for="card in art"
+                   v-for="card in events"
                    :key="card.title"
                    :cols="mobile ? 12 : 4">
 
                 <v-card>
                     <v-img :src="card.src"
-                           class="white--text align-end">
-                        <v-card-title class=" black--text" v-text="card.title"></v-card-title>
+                           class="white--text align-end"
+                           gradient="to bottom, rgba(0,0,0,.05), rgba(0,0,0,.2)">
+                        <v-card-title v-text="card.title"></v-card-title>
                     </v-img>
                 </v-card>
             </v-col>
@@ -95,20 +96,21 @@
                 mobile: false,
                 logo: require('../assets/gallery/chassis.jpg'),
                 testing: [
-                    { title: 'Sam with the Fifth Leg', src: require('../assets/year3/gallery/construction/sam_touching_5th_leg.jpeg'), flex: 4 },
-                    { title: 'Walking on the stand', src: require('../assets/gallery/walking_on_stand.jpg'), flex: 4 },
-                    { title: 'Bot standing', src: require('../assets/gallery/rambot_standing.jpg'), flex: 4 },    
-                    { title: 'Working on the robot', src: require('../assets/gallery/assembling_the_bot.jpg'), flex: 4 },
-                    { title: 'Using the oscilloscope', src: require('../assets/gallery/oscilloscope_with_EIR.jpg'), flex: 4 },
-                    { title: 'Leg programming setup', src: require('../assets/gallery/leg_programming_setup.jpg'), flex: 4 },            
+                    { title: '1st Walk Test', src: require('../assets/year3/gallery/testing/firstWalkTest.jpg'), flex: 4 },
+                    { title: 'Measuring for New Stand', src: require('../assets/year3/gallery/testing/measurements.jpg'), flex: 4 },
+                    { title: 'Testing the 5th Leg', src: require('../assets/year3/gallery/testing/testing5thLeg.jpeg'), flex: 4 }   
                 ],
                 construction: [
                     { title: 'Disassembled', src: require('../assets/year3/gallery/construction/dissassembled.jpg'), flex: 4 },
-                    { title: 'Sam with the Fifth Leg', src: require('../assets/year3/gallery/construction/sam_touching_5th_leg.jpeg'), flex: 4 },        
+                    { title: 'Fixing a Leg', src: require('../assets/year3/gallery/construction/fixingLeg.jpg'), flex: 4 },
+                    { title: 'Fifth Leg Layout', src: require('../assets/year3/gallery/construction/5thLeg.jpg'), flex: 4 },
+                    { title: 'Coding the Tennis Ball Tracking', src: require('../assets/year3/gallery/construction/mlCoding.jpg'), flex: 4 }
                 ],
                 events: [
-                    { title: 'Sparky meeting President Amy Parsons', src: require('../assets/year3/gallery/events/sparky_meets_amy_parsons.jpg'), flex: 4 },
-                    { title: 'Sparky meeting Cam the Ram', src: require('../assets/year3/gallery/events/sparky_meets_cam.jpg'), flex: 4},
+                    { title: 'Sparky meeting Amy Parsons', src: require('../assets/year3/gallery/events/amyParsons.jpg'), flex: 4 },
+                    { title: 'Sparky meeting Cam the Ram', src: require('../assets/year3/gallery/events/sparkyCam.jpg'), flex: 4 },
+                    { title: 'Demonstration', src: require('../assets/year3/gallery/events/Demonstrate.jpg'), flex: 4 },
+                    { title: 'ECE Nights', src: require('../assets/year3/gallery/events/ECENight.jpeg'), flex:4 }
                 ],
                 team: [
                     { title: 'Year 3 Team Seniors', src: require('../assets/year3/team/year3_seniors.jpg'), flex: 6 },
