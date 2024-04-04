8fea5ee5a5135422750a6009d734148dce95ec16
SuperDuper342 <lawrencemilo342@gmail.com>
2024-04-04 16:49:46 -0600

    fixed the centering

--- src/components/TeamYear3.vue
+++ src/components/TeamYear3.vue
@@ -3,10 +3,11 @@
     <v-card v-if="!this.mobile" outlined color="transparent" :max-width="1500" elevation="0" class="pa-8 ma-4">
 
         <v-row class="text-center">
-            <p class="font-weight-medium text-h4 text-center mt-4">
+            <v-col class="font-weight-medium text-h4 text-center mt-4">
                 <span style="text-decoration: underline;">Seniors</span>
-            </p>
+            </v-col>
         </v-row>
+    
         <v-row class="text-center">
             <v-col class="text-center d-flex justify-space-around"
                    v-for="card in cards"
@@ -49,9 +50,9 @@
         </v-row>
 
         <v-row class="text-center">
-            <p class="font-weight-medium text-h4 text-center mt-4">
+            <v-col class="font-weight-medium text-h4 text-center mt-4">
                 <span style="text-decoration: underline;">VIPs</span>
-            </p>
+            </v-col>
         </v-row>
 
         <v-row class="text-center">
