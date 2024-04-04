7699ba740aadde06acf24c1f4c5883823c834d4a
SuperDuper342 <lawrencemilo342@gmail.com>
2024-04-03 22:16:19 -0600

    changes

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,127 +1,57 @@
 <template>
 
 
-    <v-card color="white" elevation="2" class="pa-8 ma-4">
 
-        <p class="font-weight-medium text-h4 text--primary mt-4 text-center">Testing</p>
-        <v-row class="text-center mb-12">
-            <v-col class="text-center d-flex justify-space-around"
-                   v-for="card in testing"
-                   :key="card.title"
-                   :cols="mobile ? 12 : 4">
-
-                <v-card>
-                    <v-img :src="card.src"
-                           class="white--text align-end"
-                           gradient="to bottom, rgba(0,0,0,.05), rgba(0,0,0,.2)">
-                        <v-card-title v-text="card.title"></v-card-title>
-                    </v-img>
+    <v-row>
+        <v-col cols="1">
+            <v-spacer></v-spacer>
+        </v-col>
+        <v-col cols="this.mobile ? 12 : 10" class="d-flex justify-center align-center">
+            <v-card max-width="1300px" class="mt-4 pa-4">
+                <v-tabs @change="console.log(tab)" v-model="tab" color="#1E4D2B">
+                    <v-tab >2023/2024</v-tab>
+                    <v-tab >2022/2023</v-tab>
+                </v-tabs>
+                <v-divider></v-divider>
+                <v-card outlined color="transparent" class="d-flex justify-center align-center">
+                    <GalleryYear3 v-if="tab == 0" />
+                    <GalleryYear2 v-if="tab == 1" />
                 </v-card>
-            </v-col>
-        </v-row>
+            </v-card>
+        </v-col>
 
-        <p class="font-weight-medium text-h4 text--primary mt-4 text-center">Construction</p>
-        <v-row class="text-center mb-12">
-            <v-col class="text-center d-flex justify-space-around"
-                   v-for="card in construction"
-                   :key="card.title"
-                   :cols="mobile ? 12 : 4">
-
-                <v-card>
-                    <v-img :src="card.src"
-                           class="white--text align-end"
-                           gradient="to bottom, rgba(0,0,0,.05), rgba(0,0,0,.2)">
-                        <v-card-title v-text="card.title"></v-card-title>
-                    </v-img>
-                </v-card>
-            </v-col>
-        </v-row>
+        <v-col cols="1">
+            <v-spacer></v-spacer>
+        </v-col>
+    </v-row>
 
 
-
-        <p class="font-weight-medium text-h4 text--primary mt-12 text-center">Art</p>
-        <v-row class="text-center mb-12">
-            <v-col class="text-center d-flex justify-space-around"
-                   v-for="card in art"
-                   :key="card.title"
-                   :cols="mobile ? 12 : 4">
-
-                <v-card>
-                    <v-img :src="card.src"
-                           class="white--text align-end">
-                        <v-card-title class=" black--text" v-text="card.title"></v-card-title>
-                    </v-img>
-                </v-card>
-            </v-col>
-        </v-row>
-
-
-        <p class="font-weight-medium text-h4 text--primary mt-12 text-center">Team</p>
-        <v-row class="text-center  mb-12">
-            <v-col class="text-center d-flex justify-space-around"
-                   v-for="card in team"
-                   :key="card.title"
-                  :cols="mobile ? 12 : 6">
-
-                <v-card>
-                    <v-img 
-                           :src="card.src"
-                           class="white--text align-end"
-                           :height="mobile ? 300 : 600"
-                           gradient="to bottom, rgba(0,0,0,.05), rgba(0,0,0,.2)">
-                        <v-card-title v-text="card.title"></v-card-title>
-                    </v-img>
-                </v-card>
-            </v-col>
-        </v-row>
-
-
-
-    </v-card>
-
-
-
+   
 
 </template>
 
 <script>
-
+    import GalleryYear2 from '../components/GalleryYear2.vue'
+    import GalleryYear3 from '../components/GalleryYear3.vue'
     export default {
         mounted() {
             this.mobile = window.innerWidth <= 760
+        },
+        computed: {
+            console: () => console,
+            window: () => window,
+        },
+        components: {
+            GalleryYear2,
+            GalleryYear3
         },
         data() {
             return {
                 mobile: false,
                 logo: require('../assets/gallery/chassis.jpg'),
-                testing: [
-                    { title: 'Preparing to walk', src: require('../assets/gallery/preparing_to_walk.jpg'), flex: 4 },
-                    { title: 'Walking on the stand', src: require('../assets/gallery/walking_on_stand.jpg'), flex: 4 },
-                    { title: 'Bot standing', src: require('../assets/gallery/rambot_standing.jpg'), flex: 4 },    
-                    { title: 'Working on the robot', src: require('../assets/gallery/assembling_the_bot.jpg'), flex: 4 },
-                    { title: 'Using the oscilloscope', src: require('../assets/gallery/oscilloscope_with_EIR.jpg'), flex: 4 },
-                    { title: 'Leg programming setup', src: require('../assets/gallery/leg_programming_setup.jpg'), flex: 4 },            
-                ],
-                construction: [
-                    { title: 'Chassis side view', src: require('../assets/gallery/chassis.jpg'), flex: 4 },
-                    { title: 'Chassis diagonal view', src: require('../assets/gallery/chassis2.jpg'), flex: 4 },
-                    { title: 'Assembling the robot', src: require('../assets/gallery/robot_assembly.jpg'), flex: 4 },
-                    { title: 'Programming the Pi', src: require('../assets/gallery/pi_programming.jpg'), flex: 4 },
-                    { title: 'Disassembling a leg for testing', src: require('../assets/gallery/leg_testing.jpg'), flex: 4 },
-                    { title: 'Meeting with EIR', src: require('../assets/gallery/eir_meeting.jpg'), flex: 4 },             
-                ],
-                art: [
-                    { title: 'Logos by Gwyn Tari', src: require('../assets/gallery/RamLogoLarge.png'), flex: 4 },
-                    { title: ' ', src: require('../assets/gallery/RamLogoMed.png'), flex: 4 },
-                    { title: '', src: require('../assets/gallery/RamLogoSmall.png'), flex: 4 },
-                ],
-                team: [
-                    { title: 'Year 3 Team Seniors', src: require('../assets/year3/year3_seniors.jpg'), flex: 6 },
-                    { title: 'Year 3 Full Team', src: require('../assets/year3/year3_full.jpg'), flex: 6 },
-                    { title: 'Year 2 Team Seniors', src: require('../assets/team_seniors.jpg'), flex: 6 },
-                    { title: 'Year 2 Full Team', src: require('../assets/year2/team_full.jpg'), flex: 6 },
+                tab: null,
+                url: "https://projects-web.engr.colostate.edu/ece-sr-design/AY21/outreach/",
 
-                ],
             }
         },
     }</script>
