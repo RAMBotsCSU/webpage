1e2f5235c3d1abf4c72f53ae2528fc80a13af131
unex <7575866+unex@users.noreply.github.com>
2024-09-19 15:08:27 -0600

    move team member into component so I don't have to edit everything twice

---
+++ src/components/TeamMemberCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-card class="align-self-start" width="250px">
+        <v-img :src="card.src" class="white--text align-end" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.2)"
+            height="300px">
+            <v-card-title v-text="card.title"></v-card-title>
+        </v-img>
+        <v-card outlined color="transparent">
+            <v-card-text class="grow text-left">{{ card.text }}</v-card-text>
+        </v-card>
+
+        <v-card-actions class="justify-center">
+            <v-btn icon v-if="card.resume" :href="card.resume" target="_blank">
+                <v-icon>mdi-file-account</v-icon>
+            </v-btn>
+
+            <v-btn icon v-if="card.email" :href="`mailto:${card.email}`" target="_blank">
+                <v-icon>mdi-email</v-icon>
+            </v-btn>
+
+            <v-btn icon v-if="card.linkedin" :href="card.linkedin" target="_blank">
+                <v-icon>mdi-linkedin</v-icon>
+            </v-btn>
+
+            <v-btn icon v-if="card.github" :href="`https://github.com/${card.github}`" target="_blank">
+                <v-icon>mdi-github</v-icon>
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        card: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
