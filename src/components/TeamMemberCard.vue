6a11df2cb9debe34f8f3542d864828a699672680
Unexceptional <git@unexceptional.net>
2025-01-23 16:24:45 -0700

    update team page

--- src/components/TeamMemberCard.vue
+++ src/components/TeamMemberCard.vue
@@ -1,14 +1,12 @@
 <template>
     <v-card class="align-self-start" width="250px">
-        <v-img :src="card.src" class="text-white align-end" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.2)"
+        <v-img :src="card.src" class="text-white align-end" cover gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.2)"
             height="300px">
             <v-card-title v-text="card.title"></v-card-title>
         </v-img>
-        <v-card border color="transparent">
-            <v-card-text class="grow text-left">{{ card.text }}</v-card-text>
-        </v-card>
+        <v-card-text>{{ card.text }}</v-card-text>
 
-        <v-card-actions class="justify-center">
+        <v-card-actions class="justify-center opacity-60">
             <v-btn icon v-if="card.resume" :href="card.resume" target="_blank">
                 <v-icon>mdi-file-account</v-icon>
             </v-btn>
@@ -17,7 +15,7 @@
                 <v-icon>mdi-email</v-icon>
             </v-btn>
 
-            <v-btn icon v-if="card.linkedin" :href="card.linkedin" target="_blank">
+            <v-btn icon v-if="card.linkedin" :href="`https://www.linkedin.com/in/${card.linkedin}`" target="_blank">
                 <v-icon>mdi-linkedin</v-icon>
             </v-btn>
 
