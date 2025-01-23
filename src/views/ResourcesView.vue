9f5a99eb8f72e0395a2ec55731faeaaf9ac18b6b
Unexceptional <git@unexceptional.net>
2025-01-22 22:36:30 -0700

    update ez pages

--- src/views/ResourcesView.vue
+++ src/views/ResourcesView.vue
@@ -1,67 +1,72 @@
 <template>
-
-    <v-row class="justify-center">
-        <v-col :cols="this.mobile ? 12 : 8">
-            <v-card max-width="1500" class="mt-4 pa-4">
-                <p class="text-h4 text--primary mt-0">
-                    Resources:
-                </p>
-                <v-divider></v-divider>
-
-                <v-list-item class="mt-8">
-                    
-                        <a href="https://github.com/XRobots/openDogV3"
-                           class="text-decoration-none text-h5">James Bruton's openDogV3</a>
-                        <v-list-item-subtitle class="ml-4">Source of original mechanical design</v-list-item-subtitle>
-                    
-                </v-list-item>
-
-                <v-list-item class="mt-0">
-                    
-                        <a href="https://github.com/RAMBotsCSU"
-                           class="text-decoration-none text-h5">RamBOTs GitHub Organization</a>
-                           <a href="https://github.com/RAMBotsCSU/deployment"
-                           class="text-decoration-none text-p1 ml-4 mt-2">Deployment repository</a>
-                            <a href="https://github.com/RAMBotsCSU/testing"
-                           class="text-decoration-none text-p1 ml-4 mt-2">Testing repository</a>
-                           <a href="https://github.com/RAMBotsCSU/machine_learning"
-                           class="text-decoration-none text-p1 ml-4 mt-2">Machine Learning repository</a>
-                           <a href="https://github.com/RAMBotsCSU/source"
-                           class="text-decoration-none text-p1 ml-4 mt-2">Source repository</a>
-                           <a href="https://github.com/RAMBotsCSU/outreach"
-                           class="text-decoration-none text-p1 ml-4 mt-2">Outreach repository</a>
-                           <a href="https://github.com/RAMBotsCSU/webpage"
-                           class="text-decoration-none text-p1 ml-4 mt-2">Webpage repository</a>
-                    
-                </v-list-item>
-
-                <v-list-item>
-                    
-                        <a href="https://projects-web.engr.colostate.edu/ece-sr-design/AY23/RamBOTs" open
-                           class="text-decoration-none text-h5">RamBOTs 2023-2024 Team's Website</a>
-                           <v-list-item-subtitle class="ml-4">Check out the previous year's website!</v-list-item-subtitle>
-                    
-                </v-list-item>
-
-            </v-card>
-        </v-col>
-    </v-row>
-
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-card>
+                    <v-card-title>
+                        <span class="font-weight-black">Resources</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-list>
+                            <v-list-item>
+                                <v-list-item-title>
+                                    <a href="https://github.com/XRobots/openDogV3" class="text-decoration-none">
+                                        James Bruton's openDogV3
+                                    </a>
+                                </v-list-item-title>
+                                <v-list-item-subtitle class="ml-4">
+                                    Source of original mechanical design
+                                </v-list-item-subtitle>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-title>
+                                    <a href="https://github.com/RAMBotsCSU" class="text-decoration-none">
+                                        RamBOTs GitHub Organization
+                                    </a>
+                                </v-list-item-title>
+                                <v-list-item-subtitle>
+                                    <a href="https://github.com/RAMBotsCSU/deployment" class="text-decoration-none text-p1 ml-4 mt-2">
+                                        Deployment repository
+                                    </a>
+                                    <a href="https://github.com/RAMBotsCSU/testing" class="text-decoration-none text-p1 ml-4 mt-2">
+                                        Testing repository
+                                    </a>
+                                    <a href="https://github.com/RAMBotsCSU/machine_learning" class="text-decoration-none text-p1 ml-4 mt-2">
+                                        Machine Learning repository
+                                    </a>
+                                    <a href="https://github.com/RAMBotsCSU/source" class="text-decoration-none text-p1 ml-4 mt-2">
+                                        Source repository
+                                    </a>
+                                    <a href="https://github.com/RAMBotsCSU/outreach" class="text-decoration-none text-p1 ml-4 mt-2">
+                                        Outreach repository
+                                    </a>
+                                    <a href="https://github.com/RAMBotsCSU/webpage" class="text-decoration-none text-p1 ml-4 mt-2">
+                                        Webpage repository
+                                    </a>
+                                </v-list-item-subtitle>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-title>
+                                    <a href="https://projects-web.engr.colostate.edu/ece-sr-design/AY23/RamBOTs" class="text-decoration-none">
+                                        RamBOTs 2023-2024 Team's Website
+                                    </a>
+                                </v-list-item-title>
+                                <v-list-item-subtitle class="ml-4">
+                                    Check out the previous year's website!
+                                </v-list-item-subtitle>
+                            </v-list-item>
+                        </v-list>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-
-    export default {
-
-        mounted() {
-            this.mobile = window.innerWidth <= 760
-        },
-        data() {
-            return {
-                mobile: false,
-                logo: require('../assets/gallery/chassis.jpg'),
-
-            }
-        },
-    }
+export default {
+    data() {
+        return {};
+    },
+}
 </script>
--- src/views/TimelineView.vue
+++ src/views/TimelineView.vue
@@ -1,39 +1,23 @@
 <template>
-
-    <v-row>
-        <v-col cols="2">
-            <v-spacer></v-spacer>
-        </v-col>
-        <v-col :cols="this.mobile ? 12 : 8">
-            <v-card max-width="1800" class="mt-4 pa-4">
-                <p class="text-h4 text--primary mt-4">
-                    Project Timeline
-                </p>
-                <p class="text-h4 text--primary mt-4">
-                    2024-2025 Team's Semester 1 Timeline
-                </p>
-                <p>Coming soon...</p>
-            </v-card>
-        </v-col>
-
-        <v-col cols="2">
-            <v-spacer></v-spacer>
-        </v-col>
-    </v-row>
-
+    <v-container>
+        <v-row>
+            <v-col :cols="12">
+                <v-card>
+                    <v-card-title>
+                        <span class="font-weight-black">Project Timeline</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <p>2024-2025 Team's Semester 1 Timeline</p>
+                        <p>Coming soon...</p>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-
 export default {
-    mounted() {
-        this.mobile = window.innerWidth <= 760
-    },
-    data() {
-        return {
-            mobile: false,
-            logo: require('../assets/gallery/chassis.jpg'),
-        }
-    },
+    data() { },
 }
 </script>
