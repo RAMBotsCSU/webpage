59833528ba72d63cb3416eabb743d4928d6c7b58
unex <7575866+unex@users.noreply.github.com>
2025-01-23 13:07:53 -0700

    update gallery

---
+++ src/components/GallerySection.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <v-row class="justify-center">
+            <v-col cols="12" class="text-center mt-4">
+                <v-text class="font-weight-medium text-h5">{{ title }}</v-text>
+            </v-col>
+        </v-row>
+        <v-divider class="my-4"></v-divider>
+        <v-row class="justify-center">
+            <v-col class="d-flex" v-for="card in cards" :key="card.title" :cols="$vuetify.display.mobile ? 12 : 4">
+                <v-img :src="card.src" class="text-white align-end rounded-md" cover @click="openImage(card.src)">
+                    <v-card-title v-text="card.title"></v-card-title>
+                </v-img>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        cards: Array,
+    },
+    methods: {
+        openImage(src) {
+            window.open(src, '_blank');
+        }
+    }
+}
+</script>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,57 +1,116 @@
 <template>
-
-
-
-    <v-row>
-        <v-col cols="1">
-            <v-spacer></v-spacer>
-        </v-col>
-        <v-col cols="this.mobile ? 12 : 10" class="d-flex justify-center align-center">
-            <v-card max-width="1300px" class="mt-4 pa-4">
-                <v-tabs @update:model-value="console.log(tab)" v-model="tab" color="#1E4D2B">
-                    <v-tab >2023/2024</v-tab>
-                    <v-tab >2022/2023</v-tab>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-tabs v-model="tab" color="primary">
+                    <v-tab v-for="(year, index) in years" :key="index">{{ year.title }}</v-tab>
                 </v-tabs>
-                <v-divider></v-divider>
-                <v-card border color="transparent" class="d-flex justify-center align-center">
-                    <GalleryYear3 v-if="tab == 0" />
-                    <GalleryYear2 v-if="tab == 1" />
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-card width="100%">
+                    <v-card-text>
+                        <v-window v-model="tab">
+                            <v-window-item v-for="(year, index) in years" :key="index" :value="index">
+                                <div>
+                                    <GallerySection v-for="section in year.sections" :key="section.title"
+                                        :title="section.title" :cards="section.cards" />
+                                </div>
+                            </v-window-item>
+                        </v-window>
+                    </v-card-text>
                 </v-card>
-            </v-card>
-        </v-col>
-
-        <v-col cols="1">
-            <v-spacer></v-spacer>
-        </v-col>
-    </v-row>
-
-
-
-
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-    import GalleryYear2 from '../components/GalleryYear2.vue'
-    import GalleryYear3 from '../components/GalleryYear3.vue'
-    export default {
-        mounted() {
-            this.mobile = window.innerWidth <= 760
-        },
-        computed: {
-            console: () => console,
-            window: () => window,
-        },
-        components: {
-            GalleryYear2,
-            GalleryYear3
-        },
-        data() {
-            return {
-                mobile: false,
-                logo: require('../assets/gallery/chassis.jpg'),
-                tab: null,
-                url: "https://projects-web.engr.colostate.edu/ece-sr-design/AY23/outreach/",
+import GallerySection from '../components/GallerySection.vue'
 
-            }
-        },
-    }</script>
+export default {
+    components: {
+        GallerySection
+    },
+    data() {
+        return {
+            tab: 0,
+            years: [
+                {
+                    title: '2023/2024',
+                    sections: [
+                        {
+                            title: 'Testing', cards: [
+                                { title: '1st Walk Test', src: require('@/assets/year3/gallery/testing/firstWalkTest.jpg') },
+                                { title: 'Measuring for New Stand', src: require('@/assets/year3/gallery/testing/measurements.jpg') },
+                                { title: 'Testing the 5th Leg', src: require('@/assets/year3/gallery/testing/testing5thLeg.jpeg') }
+                            ]
+                        },
+                        {
+                            title: 'Construction', cards: [
+                                { title: 'Disassembled', src: require('@/assets/year3/gallery/construction/dissassembled.jpg') },
+                                { title: 'Fixing a Leg', src: require('@/assets/year3/gallery/construction/fixingLeg.jpg') },
+                                { title: 'Fifth Leg Layout', src: require('@/assets/year3/gallery/construction/5thLeg.jpg') },
+                                { title: 'Coding the Tennis Ball Tracking', src: require('@/assets/year3/gallery/construction/mlCoding.jpg') }
+                            ]
+                        },
+                        {
+                            title: 'Events', cards: [
+                                { title: 'Sparky meeting Amy Parsons', src: require('@/assets/year3/gallery/events/amyParsons.jpg') },
+                                { title: 'Sparky meeting Cam the Ram', src: require('@/assets/year3/gallery/events/sparkyCam.jpg') },
+                                { title: 'Demonstration', src: require('@/assets/year3/gallery/events/Demonstrate.jpg') },
+                                { title: 'ECE Nights', src: require('@/assets/year3/gallery/events/ECENight.jpeg') }
+                            ]
+                        },
+                        {
+                            title: 'Team', cards: [
+                                { title: 'Year 3 Team Seniors', src: require('@/assets/year3/team/year3_seniors.jpg') },
+                                { title: 'Year 3 Full Team', src: require('@/assets/year3/team/year3_full.jpg') },
+                            ]
+                        }
+                    ]
+                },
+                {
+                    title: '2022/2023',
+                    sections: [
+                        {
+                            title: 'Testing', cards: [
+                                { title: 'Preparing to walk', src: require('@/assets/gallery/preparing_to_walk.jpg') },
+                                { title: 'Walking on the stand', src: require('@/assets/gallery/walking_on_stand.jpg') },
+                                { title: 'Bot standing', src: require('@/assets/gallery/rambot_standing.jpg') },
+                                { title: 'Working on the robot', src: require('@/assets/gallery/assembling_the_bot.jpg') },
+                                { title: 'Using the oscilloscope', src: require('@/assets/gallery/oscilloscope_with_EIR.jpg') },
+                                { title: 'Leg programming setup', src: require('@/assets/gallery/leg_programming_setup.jpg') },
+                            ]
+                        },
+                        {
+                            title: 'Construction', cards: [
+                                { title: 'Chassis side view', src: require('@/assets/gallery/chassis.jpg') },
+                                { title: 'Chassis diagonal view', src: require('@/assets/gallery/chassis2.jpg') },
+                                { title: 'Assembling the robot', src: require('@/assets/gallery/robot_assembly.jpg') },
+                                { title: 'Programming the Pi', src: require('@/assets/gallery/pi_programming.jpg') },
+                                { title: 'Disassembling a leg for testing', src: require('@/assets/gallery/leg_testing.jpg') },
+                                { title: 'Meeting with EIR', src: require('@/assets/gallery/eir_meeting.jpg') },
+                            ]
+                        },
+                        {
+                            title: 'Art', cards: [
+                                { title: 'Logos by Gwyn Tari', src: require('@/assets/gallery/RamLogoLarge.png') },
+                                { title: ' ', src: require('@/assets/gallery/RamLogoMed.png') },
+                                { title: '', src: require('@/assets/gallery/RamLogoSmall.png') },
+                            ]
+                        },
+                        {
+                            title: 'Team', cards: [
+                                { title: 'Year 2 Team Seniors', src: require('@/assets/team_seniors.jpg') },
+                                { title: 'Year 2 Full Team', src: require('@/assets/year2/team_full.jpg') },
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    },
+}
+</script>
