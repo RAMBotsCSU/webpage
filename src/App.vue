eea36f316931d6d04e092fd7a86496e10b97bd9a
Unexceptional <git@unexceptional.net>
2025-01-23 20:32:53 -0700

    update footer

--- src/App.vue
+++ src/App.vue
@@ -6,36 +6,25 @@
             <router-view />
         </v-main>
 
-        <v-footer bottom color="#1E4D2B">
-            <v-row justify="center" no-gutters>
-                <v-btn v-for="link in links" :key="link" color="white" variant="text" rounded class="my-2">
-                    {{ link }}
-                </v-btn>
-                <v-col class="py-1 text-center text-white text-overline" cols="12">
-                    <a href="https://give.colostate.edu/campaigns/40250/donations/new"
-                        class="text-white text-decoration-underline">Support our program</a>
-                    |
-                    <a href="http://www.engr.colostate.edu/ECE401/AY24_senior_design_projects.php"
-                        class="text-white text-decoration-underline">Other ECE Sr. Design projects</a>
-                    |
-                    <a href="https://www.engr.colostate.edu/ece/" class="text-white text-decoration-underline">ECE
-                        Department</a>
-                    |
-                    <a href="http://www.colostate.edu/" class="text-white text-decoration-underline">CSU main
-                        website</a>
-                </v-col>
-            </v-row>
-        </v-footer>
+        <Footer></Footer>
     </v-app>
 </template>
 
 <script>
 import NavBar from './components/Nav.vue'
+import Footer from './components/Footer.vue'
 
 export default {
     components: {
         NavBar,
+        Footer,
     },
     data: () => ({}),
 }
 </script>
+
+<style scoped>
+main {
+    min-height: calc(100vh - 120px); /* Adjust 64px to the height of your footer */
+}
+</style>
