981344a98311a463cf7ab344da2192e275fae603
unex <7575866+unex@users.noreply.github.com>
2025-01-23 20:32:53 -0700

    update footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-footer bottom color="primary">
+        <v-row justify="center" no-gutters>
+            <v-col class="py-1 text-center text-white text-overline" cols="12">
+                <a href="https://give.colostate.edu/campaigns/40250/donations/new"
+                    class="text-white text-decoration-underline">Support our program</a>
+                |
+                <a href="http://www.engr.colostate.edu/ECE401/AY24_senior_design_projects.php"
+                    class="text-white text-decoration-underline">Other ECE Sr. Design projects</a>
+                |
+                <a href="https://www.engr.colostate.edu/ece/" class="text-white text-decoration-underline">ECE
+                    Department</a>
+                |
+                <a href="http://www.colostate.edu/" class="text-white text-decoration-underline">CSU main
+                    website</a>
+            </v-col>
+        </v-row>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    data: () => ({}),
+}
+</script>
